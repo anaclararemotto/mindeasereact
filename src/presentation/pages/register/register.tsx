674cf5a61932ea2e45020edb63e70df63a146cf5
Clara <clararemotto@gmail.com>
2026-03-07 13:39:22 -0300

    feat: create resgister page

--- src/presentation/pages/register/register.tsx
+++ src/presentation/pages/register/register.tsx
@@ -1,27 +1,28 @@
 import { Button } from "@presentation/components/button/button";
 import { Input } from "@presentation/components/input/input";
-import styles from './login.styles.module.scss'
+import styles from './register.styles.module.scss'
 
-function Login() {
+function Register() {
     return (
         <>
             <div className={styles["container"]}>
                 <div className={styles["container__brand"]}>
                     <h1 className={styles["container__brand__title"]}>MindEase</h1>
-                    <h2 className={styles["container__brand__subtitle"]}>Entre com sua conta</h2>
+                    <h2 className={styles["container__brand__subtitle"]}>Cadastre-se</h2>
                 </div>
                 <div className={styles["container__input"]} >
+                    <Input type="text" placeholder="Digite seu nome" />
                     <Input type="email" placeholder="Digite seu email" />
+                    <Input type="password" placeholder="Confirme sua senha" />
                     <Input type="password" placeholder="Digite sua senha" />
-                    <Button label="Entrar" />
+                    <Button label="Cadastrar" />
                 </div>
-                <div className={styles["container__buttons"]}>
-                    <Button label="Esqueci minha senha" transparent={true} />
-                    <Button label="Ainda não possui uma conta? Cadastre-se" transparent={true} to="/register"/>
+                <div className={styles["container__button"]}>
+                    <Button label="Já possui uma conta? Login" transparent={true} to="/login"/>
                 </div>
             </div>
         </>
     );
 }
 
-export default Login;
+export default Register;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 // src/main/routes/routes.tsx
 import App from '@app/App';
 import Login from '@presentation/pages/login/login';
+import Register from '@presentation/pages/register/register';
 import { createBrowserRouter } from 'react-router-dom';
 // Importe suas páginas/templates aqui
 // import { LoginPage } from '@/presentation/pages/login'; 
@@ -13,5 +14,9 @@ export const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login/>
+  },
+  {
+    path: "/register",
+    element: <Register/>
   },
 ]);
